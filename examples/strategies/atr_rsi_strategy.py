5b25222233017846a0b60349f0d103dfcfdb4e25
lin <lin>
2020-12-18 13:11:29 +0800

    move default strategies to example

--- examples/strategies/atr_rsi_strategy.py
+++ examples/strategies/atr_rsi_strategy.py
@@ -8,22 +8,22 @@ from howtrader.app.cta_strategy import (
     BarGenerator,
     ArrayManager,
 )
+
 import pandas as pd
 import pandas_ta as ta
 
 class AtrRsiStrategy(CtaTemplate):
     """"""
 
-    author = "51bitquant"
+    author = "用Python的交易员"
 
-    atr_length = 26
-    atr_ma_length = 6
-    rsi_length = 15
+    atr_length = 22
+    atr_ma_length = 10
+    rsi_length = 5
     rsi_entry = 16
-    trailing_percent = 0.3
-    trading_money = 10000
+    trailing_percent = 0.8
+    fixed_size = 1
 
-    trading_size = 0
     atr_value = 0
     atr_ma = 0
     rsi_value = 0
@@ -32,16 +32,28 @@ class AtrRsiStrategy(CtaTemplate):
     intra_trade_high = 0
     intra_trade_low = 0
 
-    parameters = ["atr_length", "atr_ma_length", "rsi_length",
-                  "rsi_entry", "trailing_percent", "fixed_size"]
-    variables = ["trading_size", "atr_value", "atr_ma", "rsi_value", "rsi_buy", "rsi_sell"]
+    parameters = [
+        "atr_length",
+        "atr_ma_length",
+        "rsi_length",
+        "rsi_entry",
+        "trailing_percent",
+        "fixed_size"
+    ]
+    variables = [
+        "atr_value",
+        "atr_ma",
+        "rsi_value",
+        "rsi_buy",
+        "rsi_sell",
+        "intra_trade_high",
+        "intra_trade_low"
+    ]
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
         """"""
-        super(AtrRsiStrategy, self).__init__(
-            cta_engine, strategy_name, vt_symbol, setting
-        )
-        self.bg = BarGenerator(self.on_bar, 15, self.on_15min_bar)
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+        self.bg = BarGenerator(self.on_bar)
         self.am = ArrayManager()
 
     def on_init(self):
@@ -77,10 +89,6 @@ class AtrRsiStrategy(CtaTemplate):
         """
         Callback of new bar data update.
         """
-        self.bg.update_bar(bar)
-
-
-    def on_15min_bar(self, bar: BarData):
         self.cancel_all()
 
         am = self.am
@@ -88,33 +96,31 @@ class AtrRsiStrategy(CtaTemplate):
         if not am.inited:
             return
 
-        high = pd.Series(am.close_array)
+        high = pd.Series(am.high_array)
         low = pd.Series(am.low_array)
         close = pd.Series(am.close_array)
 
-        atr_array = ta.atr(high, low, close, self.atr_length)
+        atr_array = ta.atr(high, low, close, self.atr_length) #am.atr(self.atr_length, array=True)
         self.atr_value = atr_array.iloc[-1]
-
-        self.atr_ma =  ta.sma(atr_array, self.atr_ma_length).iloc[-1]
+        self.atr_ma = atr_array.iloc[-self.atr_ma_length:].mean()
         self.rsi_value = ta.rsi(close, self.rsi_length).iloc[-1]
 
         if self.pos == 0:
             self.intra_trade_high = bar.high_price
             self.intra_trade_low = bar.low_price
 
-            self.trading_size = self.trading_money/bar.close_price
             if self.atr_value > self.atr_ma:
                 if self.rsi_value > self.rsi_buy:
-                    self.buy(bar.close_price + 5, self.trading_size)
+                    self.buy(bar.close_price + 5, self.fixed_size)
                 elif self.rsi_value < self.rsi_sell:
-                    self.short(bar.close_price - 5, self.trading_size)
+                    self.short(bar.close_price - 5, self.fixed_size)
 
         elif self.pos > 0:
             self.intra_trade_high = max(self.intra_trade_high, bar.high_price)
             self.intra_trade_low = bar.low_price
 
             long_stop = self.intra_trade_high * \
-                        (1 - self.trailing_percent / 100)
+                (1 - self.trailing_percent / 100)
             self.sell(long_stop, abs(self.pos), stop=True)
 
         elif self.pos < 0:
@@ -122,9 +128,10 @@ class AtrRsiStrategy(CtaTemplate):
             self.intra_trade_high = bar.high_price
 
             short_stop = self.intra_trade_low * \
-                         (1 + self.trailing_percent / 100)
+                (1 + self.trailing_percent / 100)
             self.cover(short_stop, abs(self.pos), stop=True)
 
+        self.put_event()
 
     def on_order(self, order: OrderData):
         """
