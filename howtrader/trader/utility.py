a40984df28af85e518aafa824dfe579e4c256081
51bitquant <stephen0823@aliyun.com>
2021-01-15 10:44:50 +0800

    fix the volume error

--- howtrader/trader/utility.py
+++ howtrader/trader/utility.py
@@ -274,7 +274,7 @@ class BarGenerator:
 
         # Update close price/volume into window bar
         self.window_bar.close_price = bar.close_price
-        self.window_bar.volume += int(bar.volume)
+        self.window_bar.volume += float(bar.volume)
         self.window_bar.open_interest = bar.open_interest
 
         # Check if window bar completed
